ed1222383b2aad1f31e83815307592ca16d2ee09
Volodymyr <vthehang@gmail.com>
2022-06-18 17:45:12 +0300

    added a pretty logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import config from "config";
 import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const PORT = config.get<number>("port");
 
 const app = express();
 
 app.listen(PORT, async () => {
-    console.log(`Server started on port ${PORT}`);
+    logger.info(`Server started on port ${PORT}`);
     await connect();
 });
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import config from "config";
+import logger from "./logger";
 
 function connect() {
     const dbUri = config.get<string>("dbUri");
@@ -7,10 +8,10 @@ function connect() {
     return mongoose
         .connect(dbUri)
         .then(() => {
-            console.log(`Connected to DB`);
+            logger.info(`Connected to DB`);
         })
         .catch((e) => {
-            console.error(`Could not connect to a DB`);
+            logger.error(`Could not connect to a DB`);
             process.exit(1);
         });
 }

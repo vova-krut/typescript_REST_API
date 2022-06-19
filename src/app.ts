1f7afaadc35cd004666ede428e96a29d9ace2d40
Volodymyr <vthehang@gmail.com>
2022-06-19 12:38:11 +0300

    added router and validating middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 const PORT = config.get<number>("port");
 
@@ -10,4 +11,5 @@ const app = express();
 app.listen(PORT, async () => {
     logger.info(`Server started on port ${PORT}`);
     await connect();
+    routes(app);
 });
